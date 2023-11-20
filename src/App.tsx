dc4da23cf404b6116779779a7d8611c5de7a6c50
Rishi Gurjar <41022502+rishi-gurjar@users.noreply.github.com>
2023-11-19 23:05:52 -0500

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import HomePage from './pages/HomePage';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../index.css';
+import Words from '../components/Words';
+
+export default function HomePage() {
+    return (
+        <div>      
+            <Words />      
+        </div>
+    )
+}
+
